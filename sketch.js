3284021d3baa373f6238ad03184d7055b662bfcb
serena3151 <77529411+serena3151@users.noreply.github.com>
2021-02-14 12:10:45 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var canvas;
+var music;
+var rect1;
+var rect2;
+var rect3;
+var rect4;
+var box;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+createCanvas(800,600);
+
+    rect1 = createSprite(200,200,30,20);
+    rect1.shapeColor = 'red'
+
+drawSprites();
+
+    //create box sprite and give velocity
+
+}
+
+function draw() {
+    background("green"));
+    //create edgeSprite
+
+
+
+    //add condition to check if box touching surface and make it box
+}
